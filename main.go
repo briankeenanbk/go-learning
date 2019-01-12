773953f7f214c71c56dfeb721f85326629007707
brian <briankeenan.bk@gmail.com>
2019-01-12 17:40:54 +0000

    added struct and interface

--- main.go
+++ main.go
@@ -2,7 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("hello")
+type speakable interface {
+	talk()
+}
 
+type person struct {
+	name   string
+	saying string
+}
+
+func (p person) talk() {
+	fmt.Println(p.name, "says", p.saying)
+}
+
+func talk(p speakable) {
+	p.talk()
+}
+
+func main() {
+	p := person{"brian", "hello"}
+	p.talk()
+	talk(p)
 }
